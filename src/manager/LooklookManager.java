fc826ae617efe1b86970dbf1bbbbe6d21ea3110d
sj66550393 <450275798@qq.com>
2018-03-01 23:58:25 +0800

    看看赚

--- src/manager/LooklookManager.java
+++ src/manager/LooklookManager.java
@@ -1,9 +1,22 @@
 package manager;
 
+import util.AdbUtils;
+import util.PicCompareUtils;
+
 public class LooklookManager {
 
 	public LooklookManager(){
 		
+	}
+	public boolean checkClickBottomGame(){
+		String path = AdbUtils.ScreenCapAndCut(249, 1106, 42, 42);
+		float sim = PicCompareUtils.comparePicByFingerPrint("res/application_selected.png", path);
+		System.out.printf("checkClickBottomGame sim=%f\n", sim);
+		if (sim > 0.95) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	public boolean checkClickHotNews(){
 		return true;
